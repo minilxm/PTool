3911d4a3c6972edc5150e63c4b6fa127366f1dd4
minilxm <34873352+minilxm@users.noreply.github.com>
2018-01-25 22:04:32 +0800

    添加新界面

---
+++ PTool/PressureForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace PTool
+{
+    partial class PressureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PressureForm));
+            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpTitle = new System.Windows.Forms.TableLayoutPanel();
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.picSetting = new System.Windows.Forms.PictureBox();
+            this.tlpParameter = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpMain.SuspendLayout();
+            this.tlpTitle.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSetting)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlpMain
+            // 
+            this.tlpMain.ColumnCount = 1;
+            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMain.Controls.Add(this.tlpTitle, 0, 0);
+            this.tlpMain.Controls.Add(this.tlpParameter, 0, 1);
+            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpMain.Location = new System.Drawing.Point(0, 0);
+            this.tlpMain.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpMain.Name = "tlpMain";
+            this.tlpMain.RowCount = 3;
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 84F));
+            this.tlpMain.Size = new System.Drawing.Size(1133, 735);
+            this.tlpMain.TabIndex = 0;
+            // 
+            // tlpTitle
+            // 
+            this.tlpTitle.ColumnCount = 4;
+            this.tlpTitle.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tlpTitle.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tlpTitle.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpTitle.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpTitle.Controls.Add(this.picLogo, 0, 0);
+            this.tlpTitle.Controls.Add(this.lbTitle, 1, 0);
+            this.tlpTitle.Controls.Add(this.picSetting, 3, 0);
+            this.tlpTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpTitle.Location = new System.Drawing.Point(0, 0);
+            this.tlpTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpTitle.Name = "tlpTitle";
+            this.tlpTitle.RowCount = 1;
+            this.tlpTitle.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpTitle.Size = new System.Drawing.Size(1133, 44);
+            this.tlpTitle.TabIndex = 0;
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Noto Sans CJK SC Regular", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lbTitle.ForeColor = System.Drawing.Color.Black;
+            this.lbTitle.Location = new System.Drawing.Point(53, 11);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(86, 21);
+            this.lbTitle.TabIndex = 1;
+            this.lbTitle.Text = "压力测试1.0";
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = global::PTool.Properties.Resources.icon_logo;
+            this.picLogo.Location = new System.Drawing.Point(5, 5);
+            this.picLogo.Margin = new System.Windows.Forms.Padding(5);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(40, 34);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            // 
+            // picSetting
+            // 
+            this.picSetting.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.picSetting.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picSetting.Image = global::PTool.Properties.Resources.icon_setting;
+            this.picSetting.Location = new System.Drawing.Point(1088, 5);
+            this.picSetting.Margin = new System.Windows.Forms.Padding(5);
+            this.picSetting.Name = "picSetting";
+            this.picSetting.Size = new System.Drawing.Size(40, 34);
+            this.picSetting.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picSetting.TabIndex = 2;
+            this.picSetting.TabStop = false;
+            // 
+            // tlpParameter
+            // 
+            this.tlpParameter.ColumnCount = 9;
+            this.tlpParameter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpParameter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpParameter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpParameter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpParameter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpParameter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpParameter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpParameter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpParameter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpParameter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpParameter.Location = new System.Drawing.Point(0, 44);
+            this.tlpParameter.Margin = new System.Windows.Forms.Padding(0);
+            this.tlpParameter.Name = "tlpParameter";
+            this.tlpParameter.RowCount = 1;
+            this.tlpParameter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpParameter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpParameter.Size = new System.Drawing.Size(1133, 73);
+            this.tlpParameter.TabIndex = 1;
+            // 
+            // PressureForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1133, 735);
+            this.Controls.Add(this.tlpMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PressureForm";
+            this.Text = "PressureForm";
+            this.Load += new System.EventHandler(this.PressureForm_Load);
+            this.tlpMain.ResumeLayout(false);
+            this.tlpTitle.ResumeLayout(false);
+            this.tlpTitle.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSetting)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpMain;
+        private System.Windows.Forms.TableLayoutPanel tlpTitle;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.PictureBox picSetting;
+        private System.Windows.Forms.TableLayoutPanel tlpParameter;
+    }
+}
---
+++ PTool/PressureForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTool
+{
+    public partial class PressureForm : Form
+    {
+        public PressureForm()
+        {
+            InitializeComponent();
+            InitUI();
+        }
+
+      
+        private void PressureForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitUI()
+        {
+            lbTitle.ForeColor = Color.FromArgb(3, 116, 214);
+            tlpParameter.BackColor = Color.FromArgb(19, 113, 185);
+        }
+
+    }
+}
